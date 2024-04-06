9bc70e3c1c9b0a5c3e4fd13edec3646f6a144375
Wellington <rodrigueswellington3@gmail.com>
2024-04-06 15:08:10 -0300

    Criado Formulário para cadastro de pessoas

--- novohabito.php
+++ novohabito.php
@@ -1,13 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/styles.css">
   <title>Lista de hábitos</title>
 </head>
-<body>
-  
 
+<body>
+  <div class="center">
+    <h1>Novo Hábito</h1>
+
+    <!--Formualário para cadastro de pessoas-->
+    <form id="formulario" action="inserthabito.php">
+      <p><label> Nome: <input type="text" id="nome" name="nome" autofocus /></label></p>
+      <p><input type="submit" value="Criar"></p>
+    </form>
+  </div>
+
+  <script type="text/javascript">
+    //Declara uma função para validar o formulário
+    const validaForm = function() {
+      let nomeHabito = document.getElementById("nome").value;
+      if ("" == nomeHabito) {
+        alert("É necessário informar o nome do Hábito");
+      }
+      return false;
+    }
+
+    //Aqui declaramos que está função ocorre sempre no submit do formulário
+    document.getElementById("formulario").onsubmit = validaForm;
+  </script>
 </body>
+
 </html>
